bb3782a6a2465ac51fcc61d93aab54c952502031
Michelle Brytowski <mn.brytowski@gmail.com>
2019-01-20 17:29:51 -0700

    add readme

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("easy-table");
-// require("console.table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -56,12 +55,7 @@ function promptPurchase(inventory) {
         filter: Number
 
     }]).then(function (val) {
-        // console.log("Purchase value: ", val)
-        // console.log("WHAT TYPE IS THIS????...", typeof val.id);
-        // console.log("Inventory: ", inventory)
-        // console.log("WHAT IS THISSSSSSSS?!?!....", val.id);
         var item = getID(val.id, inventory);
-        // console.log("Purchase function: ", item)
 
         if (item) {
             chooseAmount(item);
@@ -73,34 +67,24 @@ function promptPurchase(inventory) {
         }
 
     })
-}
+};
 
-//only getting a correct response when I input the number of rows in the database, currently only responds to '8'
-
+//Validates the user chosen item ID
 function getID(val, inventory) {
     var data;
-    // console.log("ID function: " + val)
-    // console.log(typeof val)
-    // console.log(inventory)
     for (var i = 0; i < inventory.length; i++) {
-        // console.log(inventory[i].item_id)
-        // console.log("HAYYYYY.... ", inventory[i].item_id);
         if (inventory[i].item_id === val) {
-            // console.log("Match: ", inventory[i].item_id)
             data = inventory[i];
-            return data;
-            // console.log("Data: ", data); 
+            return data; 
         } else {
-            // console.log("None")
             data = null;
         }
     }
     return data;
 };
 
+//Prompts user to choose the amount of the selected item
 function chooseAmount(item) {
-    console.log("Item:", item.item_id)
-    console.log("Stock Quantity: " + item.stock_quantity)
     inquirer.prompt([{
         name: "amount",
         type: "input",
@@ -110,7 +94,6 @@ function chooseAmount(item) {
         },
         filter: Number
     }]).then(function (val) {
-        console.log("Value: ", val.amount);
         if (item.stock_quantity > val.amount) {
         connection.query("UPDATE products SET ? WHERE ?",
         [{
@@ -121,8 +104,9 @@ function chooseAmount(item) {
         function(error) {
             if (error) throw err;
           });
-
+          console.log("------------------------------------------------");
           console.log("Thank you for your purchase! Your total is $" + val.amount * item.price);
+          console.log("------------------------------------------------");
           closeCart();
         } else {
             console.log("-------------------------------------------------");
@@ -133,6 +117,7 @@ function chooseAmount(item) {
     });
 };
 
+//Gives user choice to select more items or to quit the program
 function closeCart() {
     inquirer.prompt([{
         name: "end",
